d39918577f80b68a411664e505fd54cde8304c96
MariiaPiili <mariia.piili@student.lab.fi>
2024-10-17 11:35:33 +0300

    commet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo: '/hello', pathMatch: 'full' },
+    { path: 'hello', component: HelloWorldComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
