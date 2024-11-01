6321f2ae593937487b62e547b9e760d657154043
MariiaPiili <mariia.piili@student.lab.fi>
2024-11-01 13:27:01 +0200

    homepage

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css'
 })
 export class HelloWorldComponent {
-  title: string="First Exercise";
-  
+  title: string="Harjoittelua";
+  text: string = "Homepage";
+  itemImageUrl= "https://cdn.pixabay.com/photo/2018/10/16/09/11/pens-3750998_1280.jpg";
 
+  onButtonClicked() {
+    if (this.itemImageUrl === "https://cdn.pixabay.com/photo/2018/10/16/09/11/pens-3750998_1280.jpg") {
+      this.itemImageUrl = "https://cdn.pixabay.com/photo/2018/08/08/12/59/school-3592121_1280.jpg";
+    } else {
+      this.itemImageUrl = "https://cdn.pixabay.com/photo/2018/10/16/09/11/pens-3750998_1280.jpg";
+    };
+  }
+
+  
 }
