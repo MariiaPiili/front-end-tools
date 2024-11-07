c62d60e63c323462de99cd8801a3f25d2c3110c3
MariiaPiili <mariia.piili@student.lab.fi>
2024-11-07 10:45:05 +0200

    calculator, feedback added

---
+++ src/app/feedback/feedback.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { Question } from '../models/question';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [MatSliderModule, MatCardModule, CommonModule, FormsModule],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.css',
+})
+export class FeedbackComponent {
+  title: string = 'Feedback';
+
+  questions: Question[] = [
+    new Question('Question 1', 'Ei vielä arvosteltu'),
+    new Question('Question 2', 'Ei vielä arvosteltu'),
+    new Question('Question 3', 'Ei vielä arvosteltu'),
+  ];
+
+  onInputChange(event: any, question: Question) {
+    const value = event.target.value;    
+    if (value == 1) {
+      question.questionAnswer = 'Huono';
+    } else if (value == 2) {
+      question.questionAnswer = 'OK';
+    } else if (value == 3) {
+      question.questionAnswer = 'Hyvä';
+    } else if (value == 4) {
+      question.questionAnswer = 'Erinomainen';
+    }
+  }
+}
---
+++ src/app/models/question.ts
@@ -0,0 +1,14 @@
+export class Question {
+    questionTitle: string;
+    questionAnswer: string;
+
+    constructor(n: string, a: string){
+        this.questionTitle = n;
+        this.questionAnswer = a;
+    }
+}
+
+
+
+
+    
