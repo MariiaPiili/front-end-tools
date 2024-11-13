802f90dd1f542bddccef1e19078dde5a902b512f
MariiaPiili <mariia.piili@student.lab.fi>
2024-11-13 15:00:58 +0200

    template-driven-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { HelloWorldComponent } from "./hello-world/hello-world.component";
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 export const routes: Routes = [
     { path:"", redirectTo: '/hello', pathMatch: 'full' },
     { path: 'hello', component: HelloWorldComponent },
     { path: 'calculator', component: CalculatorComponent },
     { path: 'feedback', component: FeedbackComponent },
+    { path: 'template', component: TemplateDrivenFormComponent},
     { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './template-driven-form.component.html',
-  styleUrl: './template-driven-form.component.css'
+  styleUrl: './template-driven-form.component.css',
 })
 export class TemplateDrivenFormComponent {
+  title: string = 'Template driven form';
 
+  user = {
+    firstName: '',
+    lastName: '',
+    personId: '',
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    termsAccepted: false,
+  };
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Submitted', this.user);
+    }
+  }
 }
